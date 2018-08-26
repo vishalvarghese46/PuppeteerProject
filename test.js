60a1eb372d90bc83e19afe43594c78926f8d1e43
sujilnt <sujilnt@gmail.com>
2018-08-26 23:49:12 +0100

    need to fix to dropdowns

--- test.js
+++ test.js
@@ -10,10 +10,35 @@ puppeteer.launch({headless: false}).then(async browser => {
     await page.click("button.btn"); // Click the login button
     await page.waitForSelector(".selection_container[id='3786']"); // waiting for dom element that has morrission url
     await page.click(".selection_container[id='3786']");  // click on the morrission url
-    // once selected we goto the setting
-    await page.waitForSelector("#top-menu");  // menu contains top menu
+    // once selected we goto the setting  in the top menu ..
+    await page.waitForSelector("#top-menu");  // wait until menu contains top menu
     await page.click("#config-item");  // clicking on the setting Icon button
+    // then select "gateays "  on the left menu .
+    await page.waitForSelector(".leftmenu");    // wait until left menu loads.
+    await page.click("#gateways-item a");  // click on the gateway item
+    // creating new gateway
+    await page.waitForSelector("#new-gateway"); // waiting for the new gateway button
+    await page.click("button#new-gateway");  //  clicking on the new gateway button .
+    // selecting weather gateway
+    await page.waitForSelector("button[data-type='weather']");// waiting for the button that represents weather field
+    await page.click("button[data-type='weather']"); // click on the weather icon
+    await page.waitForSelector("button#next-button"); // waiting for the next button .
+    await page.click("button#next-button");  // once clicked moved to the next page .
+    // moving to next page
+     /*
+     * TODO : - need to create a custom function where data can be created .
+     *
+     * */
+      // filling the names
+      await page.waitForSelector("#name");
+      await page.type("#name", "test1");
 
+      await page.waitForSelector(".select2-input") ;
+      await page.type(".select2-input", "united kingdom");
+
+      // TODO : need to figure out dropdows.
+      await page.waitForSelector("#postcode");
+      await page.type("#postcode", "CF24");
 
 // Wait until the screen changes and a node matching
 // the selector #logged-in-successfully appears,
